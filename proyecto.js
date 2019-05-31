52655d239ad7b05d1fe1a5225c52e52e50e059ef
Melanie <melgiacchetta@outlook.com>
2019-05-31 16:39:07 -0300

    lupa y nada mas

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-  document.querySelector("#botonBuscar").onclick = function() {
+    document.querySelector("#botonBuscar").onclick = function() {
     document.querySelector("#Buscador").classList.add('animated', 'slideInRight')
     document.querySelector("#Buscador").style.display = "inline"
+    }
   }
-}
