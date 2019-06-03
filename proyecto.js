f29968077dea64e7563800a6ea238a94fc809716
Ema <ejuarez@udesa.edu.ar>
2019-06-03 14:44:28 -0300

    cambios en header y fondo

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,9 @@
 window.onload = function() {
     document.querySelector("#botonBuscar").onclick = function() {
-    document.querySelector("#Buscador").classList.add('animated', 'slideInRight')
+    document.querySelector("#Buscador").classList.add('animated', 'slideInLeft')
     document.querySelector("#Buscador").style.display = "inline"
+    }
+    var registro = function (){
+      document.querySelector("")
     }
   }
