e6fd81eca32788f0014d5c1c4e9906e3d796e641
Melanie <melgiacchetta@outlook.com>
2019-06-07 15:44:34 -0300

    puntaje y populares

--- proyecto.js
+++ proyecto.js
@@ -26,12 +26,40 @@ window.onload = function() { // capturamos el boton buscar y cuando se haga clic
         .then(function(datos) {
           console.log(datos);
           var estrenos = datos.results
-
+          console.log(estrenos);
+          // despues de la img va siempre esa url
           for (var i = 0; i < estrenos.length; i++) {
-            document.querySelector(".estrenos").innerHTML += '<li' + estrenos[i].id + '>' + estrenos[i].title + '</li>'
+            document.querySelector(".estrenos").innerHTML += '<li><img src="https://image.tmdb.org/t/p/original/' + estrenos[i].poster_path + '" alt=""> <div class="uk-position-center uk-panel"></div></li>'
           }
         })
-
+        // aca empieza la vinculacion con las peliculas mejor puntuadas
+        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=ccaee37d8fbe5010cfb857e26fcce8d4&page=1")
+          .then(function(respuesta) {
+            return respuesta.json()
+          })
+          .then(function(datos) {
+            console.log(datos);
+            var puntaje = datos.results
+            console.log(puntaje);
+            // despues de la img va siempre esa url
+            for (var i = 0; i < puntaje.length; i++) {
+              document.querySelector(".puntaje").innerHTML += '<li><img src="https://image.tmdb.org/t/p/original/' + puntaje[i].poster_path + '" alt=""> <div class="uk-position-center uk-panel"></div></li>'
+            }
+          })
+          // aca empieza la vinculacion con las peliculas mas populares
+          fetch("https://api.themoviedb.org/3/movie/popular?api_key=ccaee37d8fbe5010cfb857e26fcce8d4&page=1")
+            .then(function(respuesta) {
+              return respuesta.json()
+            })
+            .then(function(datos) {
+              console.log(datos);
+              var populares = datos.results
+              console.log(populares);
+              // despues de la img va siempre esa url
+              for (var i = 0; i < populares.length; i++) {
+                document.querySelector(".populares").innerHTML += '<li><img src="https://image.tmdb.org/t/p/original/' + populares[i].poster_path + '" alt=""> <div class="uk-position-center uk-panel"></div></li>'
+              }
+            })
 
 
 
