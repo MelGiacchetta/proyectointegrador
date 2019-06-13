bd08bd4d02b9742d5b5772fdaa98215dcc27d5a9
Unknown <unknown@example.com>
2019-06-13 16:38:39 -0300

    avance con el media para celular

--- proyecto.js
+++ proyecto.js
@@ -66,13 +66,17 @@ window.onload = function() { // capturamos el boton buscar y cuando se haga clic
               }
               console.log(document.querySelector(".populares").innerHTML);
             })
-            /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-    function myFunction() {
-      var x = document.getElementById("myLinks");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-    }
+
+}
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.querySelector("#myLinks");
+  console.log(x);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
