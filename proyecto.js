cf6a4a6540ed95726be4fedb4a5b82da48a2e300
Ema <ejuarez@udesa.edu.ar>
2019-06-14 14:09:39 -0300

    cree nuevos archivos para pelicula por genero

--- proyecto.js
+++ proyecto.js
@@ -6,19 +6,7 @@ window.onload = function() { // capturamos el boton buscar y cuando se haga clic
     var registro = function (){
       document.querySelector("")
     }
-     //vinculamos la api con la página para los géneros
-    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=ccaee37d8fbe5010cfb857e26fcce8d4")
-      .then(function(respuesta) {
-        return respuesta.json()
-      })
-      .then(function(datos) {
-        console.log(datos);
-        var generos = datos.genres
-
-        for (var i = 0; i < generos.length; i++) {
-          document.querySelector(".generos").innerHTML += '<option value="' + generos[i].id + '">' + generos[i].name + '</option>'
-        }
-      }) //aca empieza la vinculación con los próximos estrenos
+      //aca empieza la vinculación con los próximos estrenos
       fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=ccaee37d8fbe5010cfb857e26fcce8d4&page=1")
         .then(function(respuesta) {
           return respuesta.json()
