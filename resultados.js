ff7437d63fc0ec319b4be08e0ac98d59b47ad089
Ema <ejuarez@udesa.edu.ar>
2019-06-19 17:09:47 -0300

    mejoras varias

--- resultados.js
+++ resultados.js
@@ -5,15 +5,15 @@ window.addEventListener("load", function(){
   console.log(idResultados);
 
   var API_KEY = "ccaee37d8fbe5010cfb857e26fcce8d4"
-  var url = "https://api.themoviedb.org/3/search/movie?api_key="+API_KEY+"&language=en-US&query="+idResultados+"&page=1&include_adult=false"
+  var url = "https://api.themoviedb.org/3/search/movie?api_key="+ API_KEY +"&language=en-US&query="+ idResultados +"&page=1&include_adult=false"
   fetch(url)
     .then(function(response){
       return response.json();
     })
-    .then(function(objetoLiteralRespuesta) {
-      console.log(objetoLiteralRespuesta);
+    .then(function(objetoLiteralResultados) {
+      console.log(objetoLiteralResultados);
       //GUARDO EL ARRAY DE PELIS
-      var arrayDePeliculas = objetoLiteralRespuesta.results
+      var arrayDePeliculas = objetoLiteralResultados.results
       // CAPTURO EL UL
       var ul = document.querySelector('.busqueda')
 
